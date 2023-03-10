cca19061c884ae6e0826389a629aed08020a41f1
Jakub Wilk <wilkjakub64@gmail.com>
2023-03-10 16:53:00 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, "src/dev/ts/index.tsx")
+    },
+    module: {
+        rules: [
+        {
+            test: /\.ts?x?$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.s?css$/i,
+            use: [
+            MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                sourceMap: true,
+                },
+            },
+            "postcss-loader",
+            "sass-loader",
+            ],
+        },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".tsx"],
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "src", "static", "js"),
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: "../css/[name].css",
+        chunkFilename: "../css/[id].css",
+        }),
+    ],
+}
---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+
+const common = require("./webpack.common.js")
+
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    hot: false,
+    host: "0.0.0.0",
+    port: 9091,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    watchFiles: [
+      path.join(__dirname, 'src/**/*.py'),
+      path.join(__dirname, 'src/**/*.html'),
+    ],
+  },
+});
